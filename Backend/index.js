ee4876d5a6a93e94390fc0b771e93ef74ffebb1c
Om Wanere <omwanere593@gmail.com>
2025-08-11 00:40:43 +0530

    trying

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,25 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+const allowedOrigins = [
+  "https://social-frontend-16lb75gew-omwaneres-projects.vercel.app",
+  "https://social-frontend-git-main-omwaneres-projects.vercel.app",
+  "https://social-frontend-git-main-omwaneres-projects.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);

8597f3b073ef2ec21343b904daf7a43c1a17929b
Om Wanere <omwanere593@gmail.com>
2025-08-10 19:14:59 +0530

    Corrections

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -67,7 +67,7 @@ const Login = () => {
         className="shadow-lg flex flex-col gap-5 p-8"
       >
         <div className="my-4">
-          <h1 className="text-center font-bold text-xl">LOGO</h1>
+          <h1 className="text-center font-bold text-xl">SOCIAL</h1>
           <p className="text-sm text-center">
             Login to see photos & videos from your friends
           </p>

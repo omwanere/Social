57015578852cd2b7dc0c97d87561151822e81a10
Om Wanere <omwanere593@gmail.com>
2025-08-14 01:57:04 +0530

    uselocation

--- Frontend/src/components/ProtectedRoutes.jsx
+++ Frontend/src/components/ProtectedRoutes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const ProtectedRoutes = ({ children }) => {
   const { user } = useSelector((store) => store.auth);
